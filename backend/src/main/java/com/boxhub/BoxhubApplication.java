99d9d3ef0ffdd8cabbb535d5e0ebb284ee2a995f
Alessandro Lo Monaco <alessandrolomonaco@Alessandros-MacBook-Air.local>
2026-07-07 17:38:25 +0200

    feat: backend Spring Boot skeleton

---
+++ backend/src/main/java/com/boxhub/BoxhubApplication.java
@@ -0,0 +1,11 @@
+package com.boxhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxhubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoxhubApplication.class, args);
+    }
+}
